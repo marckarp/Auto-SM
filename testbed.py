f382cd0ef6e2f991214418a0c6b435322cde29e7
RamVegiraju <rav3sa@virginia.edu>
2022-01-12 15:54:13 -0500

    Adding wrapper functions

--- testbed.py
+++ testbed.py
@@ -7,6 +7,7 @@ tf_versions = ["1.10.0", "1.11.0", "1.12.0", "1.13.0", "1.14.0", "1.15.0", "1.15
         "1.5", "1.6", "1.7", "1.8", "1.9", "2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6"]
 
 
+from re import I
 import subprocess
 sampStr = "testfolder"
 
@@ -22,12 +23,113 @@ print("dir command to list file and directory")
 print(returned_text)
 """
 
+"""
 #check status or existence of a file
-import subprocess
-proc = subprocess.Popen(['ls','-l'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-myset=set(proc.stdout)
-print(myset)
+list_of_files = subprocess.run(['ls', '-l'], capture_output=True, text=True)
+print(list_of_files.stdout)
+if 'query.py' in list_of_files.stdout:
+        print("found")
+else:
+        print("not created")
+#ls_lines = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE).stdout.splitlines()
+#print(ls_lines)
 #print(check_model_data)
 #if "model.tar.gz" in check_model_data:
  #       print("found")
 #rint("not found")
+
+
+list_of_files = subprocess.run(['ls', '-l'], capture_output=True, text=True)
+model_artifact = 'model.tar.gz'
+if model_artifact not in list_of_files.stdout:
+        raise ValueError("model.tar.gz file was not created")
+print("Found model.tar.gz returning model artifact")
+"""
+from platform import python_version
+import boto3
+import json
+import os
+import joblib
+import pickle
+import tarfile
+import sagemaker
+from sagemaker.estimator import Estimator
+import time
+from time import gmtime, strftime
+import subprocess
+
+
+def check_model_package():
+    list_of_files = subprocess.run(['ls', '-l'], capture_output=True, text=True)
+    model_data = 'model.tar.gz'
+    if model_data not in list_of_files.stdout:
+        raise ValueError("model.tar.gz file was not created, make sure to have provided an image_uri")
+    return True
+
+def custom_inference_package(model_data, inference_script):
+    createDirectory = "mkdir code"
+    copyInference = f"cp {inference_script} code"
+    p1 = subprocess.call(createDirectory, shell=True)
+    p2 = subprocess.call(copyInference, shell=True)
+    createZip = f"tar -cvpzf model.tar.gz ./{model_data} ./code"
+    p3 = subprocess.Popen(createZip.split(), stdout=subprocess.PIPE)
+    output, error = p3.communicate()
+
+def inference_package(model_data):
+    createZip = f"tar -cvpzf model.tar.gz ./{model_data}"
+    p3 = subprocess.Popen(createZip.split(), stdout=subprocess.PIPE)
+    output, error = p3.communicate()
+
+
+def build_model_package(model_data, inference_script=None):
+    if model_data is None:
+        raise ValueError("You need to provide the file path for the directory with your model data.")
+    if inference_script is not None:
+        custom_inference_package(model_data, inference_script)
+    else:
+        inference_package(model_data)
+    if check_model_package:
+        default_bucket = sagemaker_session.default_bucket()
+        model_artifacts = f"s3://{default_bucket}/model.tar.gz"
+        response = s3.meta.client.upload_file('model.tar.gz', default_bucket, 'model.tar.gz')
+    return model_artifacts
+
+
+"""
+def check_model_data():
+    status = subprocess.call(['test','-e',"model.tar.gz"])
+    if status != 0:
+        raise ValueError("Model.tar.gz failed to build succesfully")
+    #print("Built model.tar.gz")
+    return "model.tar.gz"
+"""
+
+"""
+#To-do: clean this function
+def build_model_package(image_uri, inference_script=None):
+    if inference_script is not None:
+        createDirectory = "mkdir code"
+        copyInference = f"cp {inference_script} code"
+        p1 = subprocess.call(createDirectory, shell=True)
+        p2 = subprocess.call(copyInference, shell=True)
+        createZip = "tar -cvpzf model.tar.gz ./0000001 ./code"
+        p3 = subprocess.Popen(createZip.split(), stdout=subprocess.PIPE)
+        output, error = p3.communicate()
+        #print("Created a model.tar.gz with a custom inference script.")
+    else:
+        createZip = "tar -cvpzf model.tar.gz ./0000001"
+        p3 = subprocess.Popen(createZip.split(), stdout=subprocess.PIPE)
+        output, error = p3.communicate()
+        #print("Created a model.tar.gz without a custom inference script.")
+    
+    list_of_files = subprocess.run(['ls', '-l'], capture_output=True, text=True)
+    model_data = 'model.tar.gz'
+    if model_data not in list_of_files.stdout:
+        raise ValueError("model.tar.gz file was not created, make sure to have provided an image_uri")
+    #print("Found model.tar.gz returning model artifact")
+    default_bucket = sagemaker_session.default_bucket()
+    #print(default_bucket)
+    model_artifacts = f"s3://{default_bucket}/model.tar.gz"
+    response = s3.meta.client.upload_file('model.tar.gz', default_bucket, 'model.tar.gz')
+    return model_artifacts
+"""
--- utils.py
+++ utils.py
@@ -11,15 +11,8 @@ import time
 from time import gmtime, strftime
 import subprocess
 
-"""
-Add code for
 
-    1. Create Model
-    2. Create Endpoint Config
-    3. Create Endpoint
-"""
-
-#Setup
+#Setup (need to automate this, sts)
 client = boto3.client(service_name="sagemaker")
 runtime = boto3.client(service_name="sagemaker-runtime")
 boto_session = boto3.session.Session()
@@ -43,63 +36,111 @@ def retrieve_image(framework_type, instance_type, framework_version):
 tf_image = retrieve_image("tensorflow","ml.m5.xlarge","2.3.0")
 
 
-def check_model_data():
-    status = subprocess.call(['test','-e',"model.tar.gz"])
-    if status != 0:
-        raise ValueError("Model.tar.gz failed to build succesfully")
-    print("Built model.tar.gz")
-    return "model.tar.gz"
+def check_model_package():
+    list_of_files = subprocess.run(['ls', '-l'], capture_output=True, text=True)
+    model_data = 'model.tar.gz'
+    if model_data not in list_of_files.stdout:
+        #Edit this to a custom exception later
+        raise ValueError("model.tar.gz file was not created, make sure to have provided an image_uri")
+    return True
 
-#To-do: figure out how to list output files with subprocess
-def build_model_package(image_uri, inference_script=None):
+def custom_inference_package(model_data, inference_script):
+    createDirectory = "mkdir code"
+    copyInference = f"cp {inference_script} code"
+    p1 = subprocess.call(createDirectory, shell=True)
+    p2 = subprocess.call(copyInference, shell=True)
+    createZip = f"tar -cvpzf model.tar.gz ./{model_data} ./code"
+    p3 = subprocess.Popen(createZip.split(), stdout=subprocess.PIPE)
+    output, error = p3.communicate()
+
+def inference_package(model_data):
+    createZip = f"tar -cvpzf model.tar.gz ./{model_data}"
+    p3 = subprocess.Popen(createZip.split(), stdout=subprocess.PIPE)
+    output, error = p3.communicate()
+
+
+def build_model_package(model_data, inference_script=None):
+    if model_data is None:
+        raise ValueError("You need to provide the file path for the directory with your model data.")
     if inference_script is not None:
-        createDirectory = "mkdir code"
-        copyInference = f"cp {inference_script} code"
-        p1 = subprocess.call(createDirectory, shell=True)
-        p2 = subprocess.call(copyInference, shell=True)
-        createZip = "tar -cvpzf model.tar.gz ./0000001 ./code"
-        p3 = subprocess.Popen(createZip.split(), stdout=subprocess.PIPE)
-        output, error = p3.communicate()
-        print("Created a model.tar.gz with a custom inference script.")
+        custom_inference_package(model_data, inference_script)
     else:
-        createZip = "tar -cvpzf model.tar.gz ./0000001"
-        p3 = subprocess.Popen(createZip.split(), stdout=subprocess.PIPE)
-        output, error = p3.communicate()
-        print("Created a model.tar.gz without a custom inference script.")
-    return p3
+        inference_package(model_data)
+    if check_model_package:
+        default_bucket = sagemaker_session.default_bucket()
+        model_artifacts = f"s3://{default_bucket}/model.tar.gz"
+        response = s3.meta.client.upload_file('model.tar.gz', default_bucket, 'model.tar.gz')
+    return model_artifacts
 
-model_artifact = build_model_package(tf_image, "inference.py")
-print(model_artifact)
-
-
+model_data = build_model_package('0000001', "inference.py")
 
 
-"""
-#Step 1: Model Creation
-model_name = "tf-test" + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
-print("Model name: " + model_name)
-create_model_response = client.create_model(
-    ModelName=model_name,
-    Containers=[
-        {
-            "Image": image_uri,
-            "Mode": "SingleModel",
-            "ModelDataUrl": model_artifacts,
-            "Environment": {'SAGEMAKER_SUBMIT_DIRECTORY': model_artifacts,
-                            'SAGEMAKER_PROGRAM': 'inference.py'} 
-        }
-    ],
-    ExecutionRoleArn=role,
-)
-print("Model Arn: " + create_model_response["ModelArn"])
+def create_model(image_uri, model_data):
+    #Step 1: Model Creation
+    model_name = "tf-autosm" + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
+    #print("Model name: " + model_name)
+    create_model_response = client.create_model(
+        ModelName=model_name,
+        Containers=[
+            {
+                "Image": image_uri,
+                "Mode": "SingleModel",
+                "ModelDataUrl": model_data,
+                "Environment": {'SAGEMAKER_SUBMIT_DIRECTORY': model_data,
+                                'SAGEMAKER_PROGRAM': 'inference.py'} 
+            }
+        ],
+        ExecutionRoleArn=role,
+    )
+    model_arn = create_model_response["ModelArn"]
+    return model_name, model_arn
 
+model = create_model(tf_image, model_data)
+model_name, model_arn = model[0], model[1]
+print("Model Name: " + model_name)
+print("Model arn: " + model_arn)
 
-def create_model(image_uri, model_data, inference_script=None):
-    return None
+#have to adjust to paramterize instance type later
+def create_epc(model_name):
+    epc_name = "tf-epc-autosm" + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
+    endpoint_config_response = client.create_endpoint_config(
+        EndpointConfigName=epc_name,
+        ProductionVariants=[
+            {
+                "VariantName": "tfvariant",
+                "ModelName": model_name,
+                "InstanceType": "ml.c5.xlarge",
+                "InitialInstanceCount": 1
+            },
+        ],
+    )
+    epc_arn = endpoint_config_response["EndpointConfigArn"]
+    return epc_name, epc_arn
 
-def create_epc():
-    return None
+epc = create_epc(model_name)
+epc_name, epc_arn = epc[0], epc[1]
+print("EPC Name: " + epc_name)
+print("EPC arn: " + epc_arn)
 
-def create_ep():
-    return None
-"""
+def monitor_ep(ep_name):
+    describe_endpoint_response = client.describe_endpoint(EndpointName=ep_name)
+    while describe_endpoint_response["EndpointStatus"] == "Creating":
+        describe_endpoint_response = client.describe_endpoint(EndpointName=ep_name)
+        print(describe_endpoint_response["EndpointStatus"])
+        time.sleep(15)
+    return describe_endpoint_response
+
+def create_ep(epc_name):
+    ep_name = "tf-ep-autosm" + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
+    create_endpoint_response = client.create_endpoint(
+        EndpointName=ep_name,
+        EndpointConfigName=epc_name,
+    )
+    ep_arn = create_endpoint_response["EndpointArn"]
+    print(monitor_ep(ep_name))
+    return ep_name, ep_arn
+
+ep_status = create_ep(epc_name)
+ep_name, ep_arn = ep_status[0], ep_status[1]
+print("Endpoint name: " + ep_name)
+print("Endpoint arn: " + ep_arn)
