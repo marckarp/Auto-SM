50be375abfcaee588c3569028c2c7bd9737b0b60
Kirit Thadaka <kirit.thadaka@gmail.com>
2022-04-15 10:22:05 -0700

    Added missing file

--- automodel/frameworks.py
+++ automodel/frameworks.py
@@ -1,6 +1,6 @@
 from json.tool import main
+import os
 from .auto_model import AutoModel
-from framework_check import check_model_artifact
 
 class SKLearnModel(AutoModel):
     """ """
@@ -8,21 +8,24 @@ class SKLearnModel(AutoModel):
         kwargs['framework'] = 'sklearn'
         assert 'version' in kwargs, "Framework version must be specified."
         assert 'role' in kwargs, "Role must be provided"
+        assert 'model_data' in kwargs, "Folder with model data must be provided"
         super().__init__(**kwargs)
 
+    def _check_artifact_(self):
+        ''' '''
+        print("Model data contains: ", os.listdir(self._model_data_))
+        files = os.listdir(self._model_data_)
+        models = []
+        for f in files:
+            if f.split('.')[-1] == 'joblib':
+                models.append(f)
+        if len(models) == 0:
+            raise ValueError("Sklearn model artifact must be of type model.joblib for SageMaker.")
+        return models
+
     def package(self):
+        self._check_artifact_()
         return super().package()
-    
-    def check_artifact(self):
-        
-        ## Before packaging
-        #check that model data is proper artifact (joblib, etc)
-        #if proper artifact check for inference script
-        #then create inference package
-        filename = 'model.tar.gz'
-        if check_model_artifact("sklearn", self._model_file_):
-            self.package()
-        raise ValueError("Sklearn model artifact must be of type model.joblib for SageMaker.")
     
 class PyTorchModel(AutoModel):
     """ """
@@ -30,9 +33,23 @@ class PyTorchModel(AutoModel):
         kwargs['framework'] = 'pytorch'
         assert 'version' in kwargs, "Framework version must be specified."
         assert 'role' in kwargs, "Role must be provided"
+        assert 'model_data' in kwargs, "Folder with model data must be provided"
         super().__init__(**kwargs)
 
+    def _check_artifact_(self):
+        ''' '''
+        print("Model data contains: ", os.listdir(self._model_data_))
+        files = os.listdir(self._model_data_)
+        models = []
+        for f in files:
+            if f.split('.')[-1] in ['.pt', '.pth']:
+                models.append(f)
+        if len(models) == 0:
+            raise ValueError("PyTorch model artifact must be of type model.pt or model.pth for SageMaker.")
+        return models
+
     def package(self):
+        self._check_artifact_()
         return super().package()
 
 class TensorflowModel(AutoModel):
@@ -41,9 +58,26 @@ class TensorflowModel(AutoModel):
         kwargs['framework'] = 'tensorflow'
         assert 'version' in kwargs, "Framework version must be specified."
         assert 'role' in kwargs, "Role must be provided"
+        assert 'model_data' in kwargs, "Folder with model data must be provided"
         super().__init__(**kwargs)
 
+    def _check_artifact_(self):
+        ''' '''
+        print("Model data contains: ", os.listdir(self._model_data_))
+        tf_files = ['assets', 'variables', 'keras_metadata.pb', 'saved_model.pb']
+        files = os.listdir(self._model_data_)
+        models = []
+        if len(files):
+            for elem in files:
+                if elem in tf_files:
+                    models.append(elem)
+        
+        if not len(models) == len(tf_files):
+            raise ValueError("Tensorfflow model folder must container ['assets', 'variables', 'keras_metadata.pb', 'saved_model.pb'] for SageMaker.")
+        return models
+
     def package(self):
+        self._check_artifact_()
         return super().package()
 
 if __name__ == "__main__":
